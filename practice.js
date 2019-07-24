94b01dbf75ecbbef8d3fc72d1f7608367f8a413f
Sean Schouten <seanmschouten@gmail.com>
2019-07-24 15:10:38 -0600

    Complete Afternoon Project

--- practice.js
+++ practice.js
@@ -19,6 +19,10 @@ var arr = [10,20,30];
 
 //Code Here
 
+function first(arr){
+  return arr[0]
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -34,6 +38,9 @@ var arr = [40,50,60];
 
 //Code Here
 
+function last(arr){
+  return arr[2]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -49,6 +56,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -64,6 +76,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+function reversedLooper(letters){
+  for(i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -79,7 +96,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder(nums){
+  let evens = []
+  for(let i = 0; i < nums.length; i++){
+     if(nums[i] % 2 === 0){
+       evens.push(nums[i])
+     }
+  } 
+  return evens
+}
 
 
 
